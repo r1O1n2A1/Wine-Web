c5fde498083b8377c028b85909ab9eecb39d64cd
M1E1H1D1I1 <mrakrouki@gmail.com>
2017-04-21 03:46:27 +0200

    Changement adresse

--- src/main/java/fr/afcepf/atod/webservice/image/ServiceImageService.java
+++ src/main/java/fr/afcepf/atod/webservice/image/ServiceImageService.java
@@ -17,7 +17,7 @@ import javax.xml.ws.WebServiceFeature;
  * Generated source version: 2.2
  * 
  */
-@WebServiceClient(name = "ServiceImageService", targetNamespace = "http://images.wine.al30.afcepf.fr", wsdlLocation = "http://localhost:28080/Wine-Utile-1.0.0/downloadUploadServices?wsdl")
+@WebServiceClient(name = "ServiceImageService", targetNamespace = "http://images.wine.al30.afcepf.fr", wsdlLocation = "http://192.168.102.70:28080/Wine-Utile-1.0.0/downloadUploadServices?wsdl")
 public class ServiceImageService
     extends Service
 {
@@ -30,7 +30,7 @@ public class ServiceImageService
         URL url = null;
         WebServiceException e = null;
         try {
-            url = new URL("http://localhost:28080/Wine-Utile-1.0.0/downloadUploadServices?wsdl");
+            url = new URL("http://192.168.102.70:28080/Wine-Utile-1.0.0/downloadUploadServices?wsdl");
         } catch (MalformedURLException ex) {
             e = new WebServiceException(ex);
         }
